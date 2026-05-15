30fb736987d0db32d4f3689334b59084acdd996f
eimy <aseuo0726@gmail.com>
2026-05-16 01:25:18 +0800

    fix: remove extra getUser code from auth.function.ts, restore deleted get-user.ts

--- apps/web/src/features/auth/functions/auth.function.ts
+++ apps/web/src/features/auth/functions/auth.function.ts
@@ -1,15 +1,6 @@
 import { createServerFn } from "@tanstack/react-start";
-
 import { createDb } from "@memos/db";
 import * as schema from "@memos/db/schema/auth.table";
-
-import { authMiddleware } from "@/middleware/auth";
-
-export const getUser = createServerFn({ method: "GET" })
-	.middleware([authMiddleware])
-	.handler(async ({ context }) => {
-		return context.session;
-	});
 
 export const getIsFirstUserFn = createServerFn({ method: "GET" }).handler(
 	async () => {
