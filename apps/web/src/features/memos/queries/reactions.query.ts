8c887b62e3fbc988b9f95c652af0549ad00c9864
eimy <aseuo0726@gmail.com>
2026-05-17 13:51:53 +0800

    feat: add reactions query hooks

---
+++ apps/web/src/features/memos/queries/reactions.query.ts
@@ -0,0 +1,25 @@
+import { queryOptions, useMutation, useQueryClient } from "@tanstack/react-query";
+import {
+	listReactionsFn,
+	type ReactionUser,
+} from "../functions/list-reactions.function";
+import { toggleReactionFn } from "../functions/toggle-reaction.function";
+
+export const reactionsQueryOptions = (contentId: string) =>
+	queryOptions({
+		queryKey: ["reactions", contentId],
+		queryFn: () => listReactionsFn({ contentId } as never),
+	});
+
+export function useToggleReaction(contentId: string) {
+	const queryClient = useQueryClient();
+
+	return useMutation({
+		mutationFn: toggleReactionFn,
+		onSettled: () => {
+			queryClient.invalidateQueries({ queryKey: ["reactions", contentId] });
+		},
+	});
+}
+
+export type { ReactionUser };
